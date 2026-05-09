e196a8bf50f2b9349ed3713cfe61e31abc89c61a
marcuslannister <marcus@lannister.cc>
2026-05-09 00:16:16 -0700

    feat: disable sis

--- lisp/init-local-macos.el
+++ lisp/init-local-macos.el
@@ -7,27 +7,27 @@
 (setq mac-command-modifier 'super)
 
 ;; Install sis
-(use-package sis
-  :ensure t
-  :config
-  ;; Configure input sources based on platform
-  (when *is-a-mac*
-    (sis-ism-lazyman-config
-     "com.apple.keylayout.ABC"
-     "im.rime.inputmethod.Squirrel.Hans")
+;; (use-package sis
+;;   :ensure t
+;;   :config
+;;   ;; Configure input sources based on platform
+;;   (when *is-a-mac*
+;;     (sis-ism-lazyman-config
+;;      "com.apple.keylayout.ABC"
+;;      "im.rime.inputmethod.Squirrel.Hans")
 
-    ;; Set cursor colors for different input sources
-    (setq sis-default-cursor-color "#b81e19") ; English input source
-    (setq sis-other-cursor-color "#b81e19") ; Other input source (change as needed)
+;;     ;; Set cursor colors for different input sources
+;;     (setq sis-default-cursor-color "#b81e19") ; English input source
+;;     (setq sis-other-cursor-color "#b81e19") ; Other input source (change as needed)
 
-    ;; Enable global modes
-    (sis-global-cursor-color-mode t)
-    (sis-global-respect-mode t)
-    (sis-global-context-mode t)
+;;     ;; Enable global modes
+;;     (sis-global-cursor-color-mode t)
+;;     (sis-global-respect-mode t)
+;;     (sis-global-context-mode t)
 
-    ;; Auto-switch to other input source when entering Evil insert mode
-    (add-hook 'evil-insert-state-entry-hook #'sis-set-other)
-    (add-hook 'evil-insert-state-exit-hook #'sis-set-english)))
+;;     ;; Auto-switch to other input source when entering Evil insert mode
+;;     (add-hook 'evil-insert-state-entry-hook #'sis-set-other)
+;;     (add-hook 'evil-insert-state-exit-hook #'sis-set-english)))
 
 (use-package appine
   :ensure t
