cfdfe09059c5cd8154293b30453f8e68ba93b115
marcuslannister <marcus@lannister.cc>
2026-05-15 06:12:18 -0700

    chore: move meow to a file

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -92,140 +92,6 @@
 ;;   :config
 ;;   (evil-collection-init))
 
-(require 'meow)
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet)
-   ;; windows
-   '("o" . delete-other-windows)
-   '("=" . split-window-right)
-   '("-" . split-window-below)
-   ;; high frequency
-   '("e" . "C-x C-e")
-   '("<SPC>" . "C-x C-s")
-   '(";" . comment-dwim)
-   '("k" . kill-this-buffer)
-   '("p" . project-find-file)
-   '("b" . switch-to-buffer)
-   '("f" . find-file)
-   '("i" . imenu)
-   '("F" . toggle-frame-maximized)
-   '("r" . recentf-open)
-   )
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("N" . meow-pop-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("P" . meow-yank-pop)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("T" . meow-till-expand)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("V" . meow-kmacro-matches)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-kmacro-lines)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("Z" . meow-pop-all-selection)
-   '("&" . meow-query-replace)
-   '("%" . meow-query-replace-regexp)
-   '("'" . repeat)
-   '("\\" . quoted-insert)
-   '("<escape>" . ignore)))
-
-(when window-system
-  (setq meow-replace-state-name-list
-        '((normal . "🅝")
-          (beacon . "🅑")
-          (insert . "🅘")
-          (motion . "🅜")
-          (keypad . "🅚")))
-  )
-(setq
- meow-esc-delay 0.001
- meow-select-on-change t
- meow-cursor-type-normal 'box
- meow-cursor-type-insert '(bar . 4)
- meow-keypad-describe-delay 0.5
- meow-keypad-leader-dispatch "C-c"
- meow-expand-hint-remove-delay 2.0)
-(meow-setup)
-(meow-setup-indicator)
-(meow-setup-line-number)
-(unless (bound-and-true-p meow-global-mode)
-  (meow-global-mode 1))
-(meow-esc-mode 1)
-
 
 (use-package undo-fu
   :ensure t
@@ -552,6 +418,8 @@ The completion candidates include the Git status of each file."
 (require 'blinko)
 
 (require 'init-local-program)
+
+(require 'init-local-meow)
 
 
 (defun ml-update-all-packages ()
