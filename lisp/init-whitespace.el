e70c2c73e1358744bfaa1e26a12e4d80fecaf556
marcuslannister <marcus@lannister.cc>
2026-05-24 02:55:15 -0700

    chore: show the tabs

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -22,5 +22,14 @@
 (global-set-key [remap just-one-space] 'cycle-spacing)
 
 
+;;; Visually mark tabs (global)
+(with-eval-after-load 'whitespace
+  (setq whitespace-style '(face tabs tab-mark))
+  (setq whitespace-display-mappings
+        '((tab-mark ?\t [?→ ?\t] [?\\ ?\t]))))
+(require 'whitespace)
+(global-whitespace-mode 1)
+
+
 (provide 'init-whitespace)
 ;;; init-whitespace.el ends here
